444b63468d989c4c1b33260ac021f2c95de7d0df
fsojudi <fsojudi@gmail.com>
2023-12-01 08:41:29 +0100

    Implement advanced login

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -20,13 +20,14 @@ test "valid signup information" do
   get signup_path
   assert_difference 'User.count', 1 do
   post users_path, params: { user: { name: "Example User", email: "user@example.com",
-                                      password:              "password",
-                                      password_confirmation: "password" } }
+                                      password: "password",password_confirmation: "password" } }
+
   end
 
     follow_redirect!
     assert_template 'users/show'
-    assert is_logged_in?
+    assert_not is_logged_in?
+    # assert is_logged_in?
 
 end
 end
